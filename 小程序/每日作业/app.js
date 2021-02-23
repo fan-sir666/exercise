28733dcf86847b4d72cec6fdb58667a0028fb191
fan-sir666 <fan_sir666@163.com>
2021-02-23 20:35:29 +0800

    添加：小程序练习

---
+++ 小程序/每日作业/app.js
@@ -0,0 +1,23 @@
+// 生成快捷键 wx-app
+App({
+  //onLaunch,onShow: options(path,query,scene,shareTicket,referrerInfo(appId,extraData))
+  onLaunch: function(options){
+    
+  },
+  onShow: function(options){
+
+  },
+  onHide: function(){
+
+  },
+  onError: function(msg){
+
+  },
+  //options(path,query,isEntryPage)
+  onPageNotFound: function(options){
+
+  },
+  globalData: {
+    
+  }
+});
